d6f54571c06b33ac7c1858ed7695ce3731ec2f95
MartzDev <155332741+MartzDev@users.noreply.github.com>
2024-08-01 09:16:36 -0500

    correccion etiquetas

--- resources/views/chirp/index.blade.php
+++ resources/views/chirp/index.blade.php
@@ -3,34 +3,33 @@
     <x-slot name="header">
         <!-- Breadcrumb -->
         <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg shadow-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
-        aria-label="Breadcrumb">
-        <ul class="inline-flex items-center">
-            <li class="inline-flex items-center">
-                <a href="{{ route('dashboard') }}"
-                    class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" class="lucide lucide-house">
-                        <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                        <path
-                            d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                    </svg>
-                    <p class="px-2">{{ Str::title(__('Dashboard')) }}</p>
-                </a>
-            </li>
+            aria-label="Breadcrumb">
+            <ul class="inline-flex items-center">
+                <li class="inline-flex items-center">
+                    <a href="{{ route('dashboard') }}"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="lucide lucide-house">
+                            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                            <path
+                                d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                        </svg>
+                        <p class="px-2">{{ Str::title(__('Dashboard')) }}</p>
+                    </a>
+                </li>
 
-            <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 9 4-4-4-4" />
-            </svg>
+                <svg class="w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4" />
+                </svg>
 
-            <li class="inline-flex items-center">
-                <p class="px-2 text-sm font-medium text-gray-500 dark:text-gray-400 first-letter:uppercase">
-                    chirp</p>
-            </li>
-        </ol>
-    </nav>l>
+                <li class="inline-flex items-center">
+                    <p class="px-2 text-sm font-medium text-gray-500 dark:text-gray-400 first-letter:uppercase">chirp
+                    </p>
+                </li>
+            </ul>
         </nav>
     </x-slot>
 </x-app-layout>
